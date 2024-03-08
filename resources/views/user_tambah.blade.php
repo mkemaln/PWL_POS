cf2fd5291e2fd7db7b5ee814b98d9a93c045ad3c
mkemaln <kemalmuhammad2509@gmail.com>
2024-03-08 20:03:24 +0700

    P4 Final, Pr 2.7 ORM

---
+++ resources/views/user_tambah.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Form Tambah Data User</h1>
+    <form action="/user/tambah_simpan" method="post">
+        {{ csrf_field() }}
+
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Masukkan Username">
+        <br>
+        <label>Nama</label>
+        <input type="text" name="nama" placeholder="Masukkan Nama">
+        <br>
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Masukkan Password">
+        <br>
+        <label>Level ID</label>
+        <input type="number" name="level_id" placeholder="Masukkan ID Level">
+        <br><br>
+        <input type="submit" value="Simpan" class="btn btn-success">
+        <a href="/user" class="btn btn-secondary mx-3">Kembali</a>
+    </form>
+</body>
+</html>
