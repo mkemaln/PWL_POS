55d0df9d74b4f9e05dadaf56183dcf7b7e7d0c94
mkemaln <kemalmuhammad2509@gmail.com>
2024-03-29 07:47:18 +0700

    7.1 - 7.4 made all user CRUD with datatable and jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,15 @@
-@extends('adminlte::page')
-@section('title', 'Dashboard')
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
+@extends('layouts.template')
 
 @section('content')
-    {{-- <div class="card-body">
-    <form>
-        <div class="row">
-            <div class="col-sm-6">
-            <!-- text input -->
-                <div class="form-group">
-                    <label>Level id</label><input type="text" class="form-control" placeholder="id">
-                </div>
-            </div>
-        </div>
-        <button type = "submit" class ="btn btn-info">Submit </button>
-    </div> --}}
-    <div class="card card-warning">
-        <div class="card-header">
-          <h3 class="card-title">Form Tambah Level</h3>
-        </div>
-        <!-- /.card-header -->
-        <div class="card-body">
-          <form>
-            <div class="row">
-              <div class="col-sm-6">
-                <!-- text input -->
-                <div class="form-group">
-                  <label>Kode Level</label>
-                  <input type="text" class="form-control" placeholder="Enter the Level Code">
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <!-- text input -->
-                <div class="form-group">
-                  <label>Nama Level</label>
-                  <input type="text" class="form-control" placeholder="Enter the Name Code">
-                </div>
-              </div>
-            </div>
-          </form>
-          <div type="submit" class="btn btn-info">Submit</div>
-        </div>
-        <!-- /.card-body -->
-      </div>
+    
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Halo, apakabar!!</h3>
+      <div class="card-tools"></div>
+    </div>
+    <div class="card-body">
+      Selamat Datang semua, ini adalah halaman utama dari aplikasi ini.
+    </div>
+  </div>
 
-    <div class="card card-secondary">
-        <div class="card-header">
-          <h3 class="card-title">Form Tambah User</h3>
-        </div>
-        <!-- /.card-header -->
-        <div class="card-body">
-          <form>
-            <div class="row">
-              <div class="col-sm-6">
-                <!-- text input -->
-                <div class="form-group">
-                  <label>Username</label>
-                  <input type="text" class="form-control" placeholder="Enter the Username">
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <!-- text input -->
-                <div class="form-group">
-                  <label>Password</label>
-                  <input type="password" class="form-control" placeholder="Enter the Password">
-                </div>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="col-sm-6">
-                <!-- select -->
-                <div class="form-group">
-                  <label>Pilih Level</label>
-                  <select class="form-control">
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </form>
-          <div type="submit" class="btn btn-info">Submit</div>
-        </div>
-        <!-- /.card-body -->
-      </div>
-@stop
-
-@section('css')
-{{-- Add here extra stylesheets --}}
-{{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
-
-@section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
-@stop
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\POSController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,14 +18,14 @@ use App\Http\Controllers\KategoriController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index']);
 
+// Page Level
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/level/create', [LevelController::class, 'create']);
 Route::post('/level', [LevelController::class, 'store']);
 
+// Page Kategori
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/kategori/create', [KategoriController::class, 'create']);
 Route::post('/kategori', [KategoriController::class, 'store']);
@@ -33,6 +34,7 @@ Route::put('/kategori/edit_save/{id}', [KategoriController::class, 'edit_save'])
 Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete'])->name('kategori.delete');
 Route::get('/kategori/delete_confirm/{id}', [KategoriController::class, 'delete_conf']);
 
+// Page User
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
@@ -42,5 +44,18 @@ Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 // jobsheet 6 CRUD
 Route::resource('m_user', POSController::class);
+
+// route jobsheet 7 
+// refined user page
+Route::group(['prefix' => 'user'], function() {
+    Route::get('/', [UserController::class, 'index']);              // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);          // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);       // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);             // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);           // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);      // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);         // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);     // menghapus data user
+});
 
 
